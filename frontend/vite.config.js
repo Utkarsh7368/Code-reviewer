6c946501614ad9147e34d31e3f0ab960130c20ed
wrms-utkarsh <utkarsh.gupta@wrmsglobal.com>
2025-06-11 12:44:22 +0530

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/ai': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    }
+  }
 })
